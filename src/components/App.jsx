14e3711407b22b45fd0100784462bdedba638deb
Mukaila Semiu <semiteagain@gmail.com>
2022-08-31 09:27:20 +0000

    Update App.jsx event handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 function App() {
-  return <div />;
+  const [todo, setTodo] = useState("")
+    
+    const handleChange = (event ) =>{
+        const newValue= event.target.value
+
+        setTodo(newValue )
+    }
+    
+  return <div>
+      <h3>Todo List</h3>
+      <input 
+      onChange={handleChange} 
+      type="text" 
+      value={todo }
+/>
+      
+  </div>;
 }
 
 export default App;
